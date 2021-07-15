d0b6910aad884b5ca6a192bdc9db199b41b6d716
1262741800@qq.com <hujie20122736>
2021-07-16 00:19:57 +0800

    分布式锁 - redisson单机配置引入

--- distribute-lock/src/main/java/com/hujie/distributelock/TestController.java
+++ distribute-lock/src/main/java/com/hujie/distributelock/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/test-lock")
 public class TestController {
 
     @Autowired
